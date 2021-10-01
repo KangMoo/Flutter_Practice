20640e5ad74cbeefcaffb9c615538b77f4f94a4d
hkm0629 <hkm0629@uangel.com>
2021-10-01 20:21:47 +0900

    update

--- practice2/lib/main.dart
+++ practice2/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,8 +12,58 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               title: const Text('Welcome to Flutter2'),
             ),
-            body: const Center(
-              child: const Text('Hello World'),
+            body: Center(
+              // child: const Text('Hello World'),
+              // child: Text(wordPair.asPascalCase),
+              child: RandomWords(),
             )));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('무한 리스트'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.separated(
+        padding: const EdgeInsets.all(16),
+        itemCount: 10,
+        separatorBuilder:(BuildContext context, int index){
+          return Divider();
+    },
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          final int index = i ~/ 2;
+          // if (index >= _suggestions.length) {
+          //   _suggestions.addAll(generateWordPairs().take(10));
+          // }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+        title: Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ));
   }
 }
