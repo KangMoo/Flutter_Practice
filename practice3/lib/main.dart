8867b83d364a89e1fb62e05326b59e128503c680
hkm0629 <hkm0629@uangel.com>
2021-10-02 23:19:26 +0900

    update

--- practice3/lib/main.dart
+++ practice3/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BBANTO',
-      home:Grade(),
+      home: Grade(),
     );
   }
 }
@@ -16,10 +16,38 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      backgroundColor: Colors.amber[800],
+      appBar: AppBar(
         title: Text('BBANTO'),
         backgroundColor: Colors.amber[700],
-      )
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+            Text('Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('BBANTO',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing:  2,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+          ],
+          ),
+      ),
     );
   }
 }
